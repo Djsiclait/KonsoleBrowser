f3097d8ebc07f5ce8a5f225eb7fc921af5b59f15
djsiclait <djidjellypas92@gmail.com>
2016-05-18 20:33:23 -0400

    Capture user input via Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import org.jsoup.*;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // Step 1 asking for URL
+        // Step 1: asking and capturing the URL
+        System.out.print("URL: ");
+
+        Scanner input = new Scanner(System.in); // setting input to "stdin"
+
+        String url = input.nextLine(); // saving user input
+
 
     }
 }
+
+
